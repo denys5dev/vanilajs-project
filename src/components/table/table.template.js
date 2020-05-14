a30cd8180ecb0fd3accb2ceab49b5642fada6320
denys5dev <denys5dev@gmail.com>
2020-05-14 16:21:08 +0200

    resize header

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -5,38 +5,47 @@ const CODES = {
 
 function toCell() {
   return /*html*/ `
-  <div class="cell" contenteditable></div>
+    <div class="cell" contenteditable></div>
   `
 }
 
 function toColumn(col) {
   return /*html*/ `
-  <div class="column">${col}</div>`
+    <div class="column" data-type="resizable">
+      ${col}
+      <div class="col-resize" data-resize="col"></div>
+    </div>
+  `
 }
 
 function createRow(index, content) {
+  const resize = index ? '<div class="row-resize" data-resize="row"></div>' : ''
   return /*html*/ `
     <div class="row">
-        <div class="row-info">${index ? index : ''}</div>
-        <div class="row-data">${content}</div>
+      <div class="row-info">
+        ${index ? index : ''}
+        ${resize}
+      </div>
+      <div class="row-data">${content}</div>
     </div>
-    `
+  `
 }
 
 function toChar(_, index) {
   return String.fromCharCode(CODES.A + index)
 }
 
-export function createTable(rowCount = 15) {
-  const colsCount = CODES.Z - CODES.A + 1
+export function createTable(rowsCount = 15) {
+  const colsCount = CODES.Z - CODES.A + 1 // Compute cols count
   const rows = []
 
   const cols = new Array(colsCount).fill('').map(toChar).map(toColumn).join('')
 
   rows.push(createRow(null, cols))
 
-  for (let i = 0; i < rowCount; i++) {
+  for (let i = 0; i < rowsCount; i++) {
     const cells = new Array(colsCount).fill('').map(toCell).join('')
+
     rows.push(createRow(i + 1, cells))
   }
 
